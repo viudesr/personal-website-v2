728f6f894f4ed00cd5422776e1a28e96e0d44730
viudesr <raphael.viudes@gmail.com>
2024-12-24 13:39:07 +0100

    Add draft gallery views

--- src/gallery/admin.py
+++ src/gallery/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+
+from .models import Article, Picture, Article_joint_picture
+
+admin.site.register(Article)
+admin.site.register(Picture)
+admin.site.register(Article_joint_picture)
--- src/gallery/views.py
+++ src/gallery/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Article
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the gallery index.")
+    latest_article_list = Article.objects.order_by("-pub_date")[:10]
+    context = {"latest_article_list": latest_article_list}
+    return render(request, "gallery/index.html", context)
+
+def detail(request, article_id):
+    article_data = Article.objects.get(pk=article_id)
+    context = {"article_data": article_data}
+    return render(request, "gallery/detail.html", context)
